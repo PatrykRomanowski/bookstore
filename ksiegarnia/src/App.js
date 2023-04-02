1898dda9b99ca82381542ae8d21a7344c039b88b
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-04-02 17:25:19 +0200

    add Header component

--- ksiegarnia/src/App.js
+++ ksiegarnia/src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import TestComponent from "./Components/test";
 import Test2Component from "./Components/test2";
 import TestComponent3 from "./Components/test3";
+import Header from "./Components/header";
 
 function App() {
   return (
     <div className="App">
+      <Header className="headerStyles" />
       <TestComponent />
       <Test2Component />
       <TestComponent3 />
---
+++ ksiegarnia/src/Components/header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+const styles = {
+  headerStyle: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100vw",
+    height: "7vh",
+    backgroundColor: "#777",
+    fontFamily: "Changa One, sans-serif",
+    color: "white",
+  },
+  title: {
+    fontSize: 33,
+    marginLeft: 12,
+  },
+  menu: {
+    display: "flex",
+  },
+  menuItem: {
+    margin: 10,
+  },
+  shoppingCard: {
+    display: "flex",
+    alignItems: "center",
+  },
+  shoppingCardText: {
+    margin: 10,
+  },
+  shoppingIcon: {
+    fontSize: 24,
+  },
+};
+
+const Header = () => {
+  return (
+    <div style={styles.headerStyle}>
+      <div style={styles.title}>KSIAZKOWNIA</div>
+      <div style={styles.menu}>
+        <div style={styles.menuItem}>SKLEP</div>
+        <div style={styles.menuItem}>PROMOCJE</div>
+        <div style={styles.menuItem}>LOGOWANIE</div>
+      </div>
+      <div style={styles.shoppingCard}>
+        <div>
+          <FaShoppingCart style={styles.shoppingIcon} />
+        </div>
+        <div style={styles.shoppingCardText}>TWÓJ KOSZYK:</div>
+        <div style={styles.shoppingCardText}>0.00 zł</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

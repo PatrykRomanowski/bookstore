4024a19d0242589a5091e6afff4b0b42f4ef7a94
Jerzy Gos <jerzygos@yahoo.co.uk>
2023-04-02 12:26:18 +0100

    test 3 created

--- ksiegarnia/src/App.js
+++ ksiegarnia/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import TestComponent from "./Components/test";
 import Test2Component from "./Components/test2";
+import TestComponent3 from "./Components/test3";
 
 function App() {
   return (
     <div className="App">
       <TestComponent />
       <Test2Component />
+      <TestComponent3 />
     </div>
   );
 }
---
+++ ksiegarnia/src/Components/test3.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";  
+
+const TestComponent3 = () => {
+    return <div> TEST 3  jurek</div>;
+  };
+  
+  export default TestComponent3;

1e1896ba50fdf79c5c4b271ead434602e335acec
Jerzy Gos <jerzygos@yahoo.co.uk>
2023-04-02 22:33:34 +0100

    component jurektest

--- ksiegarnia/src/App.js
+++ ksiegarnia/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import TestComponent from "./Components/test";
 import Test2Component from "./Components/test2";
 import TestComponent3 from "./Components/test3";
+import TestCompJurek from "./Components/jurektest";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <TestComponent />
       <Test2Component />
       <TestComponent3 />
+      <TestCompJurek />
+
     </div>
   );
 }

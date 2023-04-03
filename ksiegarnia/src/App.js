3ed54143ccb2e5c41a759e3847b2823f66009594
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-04-03 15:34:16 +0200

    add navigation

--- ksiegarnia/src/App.js
+++ ksiegarnia/src/App.js
@@ -1,18 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
-
 import TestComponent from "./Components/test";
 import Test2Component from "./Components/test2";
 import TestComponent3 from "./Components/test3";
 import Header from "./Components/header";
+import LoginComponent from "./Components/login";
 
 function App() {
   return (
-    <div className="App">
-      <Header className="headerStyles" />
-      <TestComponent />
-      <Test2Component />
-      <TestComponent3 />
-    </div>
+    <Router>
+      <div className="App">
+        <Header className="headerStyles" />
+        <Routes>
+          <Route exact path="/" element={<TestComponent />} />
+          <Route path="/test2" element={<TestComponent3 />} />
+          <Route path="/test3" element={<Test2Component />} />
+          <Route path="/test3" element={<Test2Component />} />
+          <Route path="/login" element={<LoginComponent />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- ksiegarnia/src/Components/header.js
+++ ksiegarnia/src/Components/header.js
@@ -1,54 +1,42 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 
-const styles = {
-  headerStyle: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100vw",
-    height: "7vh",
-    backgroundColor: "#777",
-    fontFamily: "Changa One, sans-serif",
-    color: "white",
-  },
-  title: {
-    fontSize: 33,
-    marginLeft: 12,
-  },
-  menu: {
-    display: "flex",
-  },
-  menuItem: {
-    margin: 10,
-  },
-  shoppingCard: {
-    display: "flex",
-    alignItems: "center",
-  },
-  shoppingCardText: {
-    margin: 10,
-  },
-  shoppingIcon: {
-    fontSize: 24,
-  },
-};
+import "./header.css";
 
 const Header = () => {
   return (
-    <div style={styles.headerStyle}>
-      <div style={styles.title}>KSIAZKOWNIA</div>
-      <div style={styles.menu}>
-        <div style={styles.menuItem}>SKLEP</div>
-        <div style={styles.menuItem}>PROMOCJE</div>
-        <div style={styles.menuItem}>LOGOWANIE</div>
-      </div>
-      <div style={styles.shoppingCard}>
-        <div>
-          <FaShoppingCart style={styles.shoppingIcon} />
+    <div className="header">
+      <div className="title">KSIAZKOWNIA</div>
+      <div className="menu">
+        <div className="menu-item">
+          <Link className="header-link" to="/test2">
+            SKLEP
+          </Link>
         </div>
-        <div style={styles.shoppingCardText}>TWÓJ KOSZYK:</div>
-        <div style={styles.shoppingCardText}>0.00 zł</div>
+        <div className="menu-item">
+          <Link className="header-link" to="/test3">
+            PROMOCJE
+          </Link>
+        </div>
+        <div className="menu-item">
+          <Link className="header-link" to="/login">
+            LOGOWANIE
+          </Link>
+        </div>
+      </div>
+      <div className="shopping-card">
+        <div>
+          <FaShoppingCart className="shopping-icon" />
+        </div>
+        <div className="shopping-card-text">TWÓJ KOSZYK:</div>
+        <div className="shopping-card-text">0.00 zł</div>
       </div>
     </div>
   );

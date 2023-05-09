70b89e6315b4f587ef2a3dc5b322d96a039a34ce
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-05-09 19:11:05 +0200

    test

--- ksiegarnia/src/Components/header.js
+++ ksiegarnia/src/Components/header.js
@@ -20,7 +20,7 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="title">KSIAZKOWNIA</div>
+      <div className="title">KSIAZKOWNIA V2</div>
       <div className="menu">
         <div className="menu-item">
           <Link className="header-link" to="/test2">

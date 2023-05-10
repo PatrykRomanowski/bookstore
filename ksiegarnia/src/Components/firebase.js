f37a110167ee13fbce0037ce263b07da88605473
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-05-10 13:47:29 +0200

    add sekret keys

--- ksiegarnia/src/Components/firebase.js
+++ ksiegarnia/src/Components/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.React_App_APIKEY,
-  authDomain: "bookstore-15bbd.firebaseapp.com",
-  projectId: "bookstore-15bbd",
-  storageBucket: "bookstore-15bbd.appspot.com",
-  messagingSenderId: "570084528056",
-  appId: "1:570084528056:web:c539febb0740c15988894d",
-  measurementId: "G-8VWRTWFP86",
+  authDomain: process.env.React_App_authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 export const app = initializeApp(firebaseConfig);
